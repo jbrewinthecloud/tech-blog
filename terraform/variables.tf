c265f441d91a62f782ca4c41e78638e35dd44986
Jermaine Brewer <chowrunner2000@gmail.com>
2025-04-17 15:58:25 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,10 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
 
