0ab1059b1a499da3c84703af8fcb194c708cd4c5
Jermaine Brewer <chowrunner2000@gmail.com>
2025-04-17 15:58:09 -0500

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,10 @@
+output "s3_bucket_name" {
+  description = "The name of the blog S3 bucket"
+  value       = aws_s3_bucket.blog.bucket
+}
+
+output "cloudfront_domain_name" {
+  description = "CloudFront Distribution domain name"
+  value       = aws_cloudfront_distribution.blog.domain_name
+}
 
